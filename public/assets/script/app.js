4c7eaf7e5340ed5e844a866811f080b8863d31ff
Kay'z.Part <particulier.flore@gmail.com>
2022-10-20 16:21:23 +0200

    feat: test

--- public/assets/script/app.js
+++ public/assets/script/app.js
@@ -10,6 +10,7 @@ fetch('./Models/Ajax.php', {
   .then((response) => { return response.json() })
   .then(datas => {
     datas.map(element => {
+      // Général
       let nombreSelectionner = element.score_param1
       let order = element.score_param2
       let help = element.score_param3
@@ -17,61 +18,95 @@ fetch('./Models/Ajax.php', {
       let random = 10
       let score = 0
       let counter = 0
-      let resultOperation = nombreSelectionner * nombre2
-      console.log(nombreSelectionner * nombre2)
-      // Si user sélectionne bille + ordre
-      function startGame(){
-        if (order == 1 && help == 1) {
-          Generate()
-          click()
-          // let nombre2 = 1
-          // Question opération
-          document.getElementById('question').innerHTML = `<span>${nombreSelectionner} x ${nombre2}</span>`
-          
-          const table = document.createElement('table');
-          for (let i = 1; i <= nombre2; i++) {
-            const row = document.createElement('tr');
-            for (let j = 1; j <= nombreSelectionner; j++) {
-              const col = document.createElement('td');
-              col.classList.add('col-header')
-              row.appendChild(col);
-            }
-            table.appendChild(row);
-          }
-          document.body.appendChild(table);
-        }
-      }
-      startGame()
+      // let resultOperation = nombreSelectionner * nombre2
+
+      let multiple = [
+        { nombreSelectionner, nombre2: 1 },
+        { nombreSelectionner, nombre2: 2 },
+        { nombreSelectionner, nombre2: 3 },
+        { nombreSelectionner, nombre2: 4 },
+        { nombreSelectionner, nombre2: 5 },
+        { nombreSelectionner, nombre2: 6 },
+        { nombreSelectionner, nombre2: 7 },
+        { nombreSelectionner, nombre2: 8 },
+        { nombreSelectionner, nombre2: 9 },
+        { nombreSelectionner, nombre2: 10 }
+      ];
+
+      console.log(multiple)
+      let resultOperations = multiple.map(function (element) {
+        const results = `${element.nombreSelectionner}` * `${element.nombre2}`
+        return results
+        // console.log(results)
+      })
+      // for(let op of resultOperations.values()){
+      //   console.log(op)
+      // }
+      const tables = document.getElementById('tables')
+      tables.innerHTML += resultOperations.join(" ")
+
+      multiple.forEach(function (item, index) {
+        console.log(item);
+      });
+
       
-      function Generate() {
-        for (let i = 1; i <= 10; i++) {
-          let result = nombreSelectionner * i
-
-          document.getElementById('choice').innerHTML += `<div class="billes"><input type="radio" name="score_valeur" id="score_valeur" value= ${result}>${result}</div>`
-          // Affichage de la table 
-          document.getElementById('table').innerHTML += `<div class="operation">${nombreSelectionner} x ${i} = ... <br ></div>`
-        }
+      for (let i = 1; i <= 10; i++) {
+        // console.log(result)
+        document.getElementById('table').innerHTML += `<div class="operation">${nombreSelectionner} x ${i} = ... <br ></div>`
+      }
+      
+      for (let i = 1; i <= 10; i++) {
+        let result = nombreSelectionner * i
+        
+        document.getElementById('choice').innerHTML += `<div class="billes"><input type="radio" name="score_valeur" id="score_valeur" value= ${result}>${result}</div>`
       }
 
+      startGame()
+      function startGame() {
+        // Question opération
+        document.getElementById('question').innerHTML = `<span>${nombreSelectionner} x ${nombre2}</span>`
+        
+        if (order == 1 && help == 1) {
+          generateBilles()
+        }
+        click()
+      }
+
+      function generateBilles() {
+        const table = document.createElement('table');
+        for (let i = 1; i <= nombre2; i++) {
+          const row = document.createElement('tr');
+          for (let j = 1; j <= nombreSelectionner; j++) {
+            const col = document.createElement('td');
+            col.classList.add('col-header')
+            row.appendChild(col);
+          }
+          table.appendChild(row);
+          break
+        }
+        document.body.appendChild(table);
+      }
+      
       function click() {
-        const billes = document.querySelectorAll('.billes')
-        console.log(billes)
         const inputScoreValeur = document.querySelectorAll('input[type=radio][name=score_valeur]')
         const valeurScore = document.querySelector('input[type=radio][name=score_valeur]')
-
 
         if (valeurScore) {
           inputScoreValeur.forEach((elem) => {
             const parent = elem.parentNode
-            console.log(parent)
             elem.addEventListener("click", function (event) {
               let item = event.target.value;
               console.log(item);
-              console.log(resultOperation)
-              if (item == resultOperation && counter < 10) {
-                parent.classList.add('true')
+              if (item == nombreSelectionner * nombre2) {
+                // parent.classList.add('true')
+                parent.style.backgroundImage = "url('public/assets/ressources/bille-vert.png')"
+                document.getElementById('result').innerHTML = `<p> Bien jouer !`
+                // document.getElementById('table').innerHTML += `<div class="operation">${nombreSelectionner} x ${nombre2} = ${nombreSelectionner * nombre2}<br ></div>`
                 score++
-                document.getElementById('result').innerHTML = `<p> Bien jouer !</p><button>Continuer</button>`
+                nombre2++
+                console.log(nombreSelectionner * nombre2)
+                startGame()
+                
               } else {
                 parent.classList.add('false')
                 document.getElementById('result').innerHTML = `<p> Essaye encore !</p>`
@@ -79,7 +114,8 @@ fetch('./Models/Ajax.php', {
               counter++
               showScore()
               // if (counter < 10) {
-              //   setTimeout(startGame, 3000)
+              //   setTimeout(startGame(), 3000)
+              //   return
               // }else{
               //   document.getElementById('score').innerHTML = ""
               //   document.getElementById('result').innerHTML = `<p> Score : ${score} / 10</p>`
@@ -93,25 +129,7 @@ fetch('./Models/Ajax.php', {
         document.getElementById('score').innerHTML = `<p> Score : ${score} / 10</p>`
       }
 
-      // let multiple = [
-      //   {nombreSelectionner, nombre2: "1"},
-      //   {nombreSelectionner, nombre2: "2"},
-      //   {nombreSelectionner, nombre2: "3"},
-      //   {nombreSelectionner, nombre2: "4"},
-      //   {nombreSelectionner, nombre2: "5"},
-      //   {nombreSelectionner, nombre2: "6"},
-      //   {nombreSelectionner, nombre2: "7"},
-      //   {nombreSelectionner, nombre2: "8"},
-      //   {nombreSelectionner, nombre2: "9"},
-      //   {nombreSelectionner, nombre2: "10"}
-      // ];
-      //  let resultOperation = multiple.map(function(element){
-      //   return `${element.nombreSelectionner}` * `${element.nombre2}`
-      //  })
-      //  console.log(resultOperation)
-
-
-      // Ordre + bille
+     
     })
   })
   .catch((error) => console.log(error))
@@ -129,3 +147,14 @@ function showScoreInformation() {
   document.getElementById('score').innerHTML = `<p>Score : 20:20</p>`
 }
 
+// Test table multiplication 
+// const array = []
+// for (let i = 1; i < 11; i++) {
+//   array.push(i)
+// }
+// for (let j = 1; j < 11; j++) {
+//   const multiplications = array.map((num) => {
+//     return num * j;
+//   })
+//   console.log(multiplications)
+// }
